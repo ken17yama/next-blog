1f4e74b3b4cac7b780189a5949154a59ebc2c165
ken17yama <ken17yama@gmail.com>
2020-07-27 23:46:41 +0900

    Typescriptを使用できるよう設定し、jsファイルをtsxファイルに変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,17 @@ import Date from '../components/date'
 import utilStyles from '../styles/utils.module.css'
 
 import { getSortedPostsData } from '../lib/posts'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -47,4 +46,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
