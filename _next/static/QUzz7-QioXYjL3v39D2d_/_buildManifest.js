8dd0e35fb548dd21bfce290a076b4d239ab2b971
ken17yama <ken17yama@users.noreply.github.com>
2020-07-29 13:41:19 +0000

    deploy: 51f812575d325093a4d3b408c7adc2d3fdf0d662

---
+++ _next/static/QUzz7-QioXYjL3v39D2d_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F92cdbd77058e7273caa332133afa26a3739ddad2.2d58d35f34f2f8fb9e77.js","static\u002Fcss\u002Fb97b8ca76f0fcb7c9bc9.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/_ugtsa7vk5d5LRih411vA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002Fe5faf235edd2d05fb4859c4fd345204cc35934f1.2d58d35f34f2f8fb9e77.js","static\u002Fcss\u002Fb97b8ca76f0fcb7c9bc9.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
