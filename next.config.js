36bdc9c0e19c3ab30a1513edeb782f08c928b387
ken17yama <ken17yama@gmail.com>
2020-07-29 22:39:37 +0900

    画像が表示されないのでbasePathを設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	basePath: process.env.NODE_ENV === "production" ? "/next-blog" : "",
 	// some configuration
 	assetPrefix: process.env.NODE_ENV === "production" ? "/next-blog" : ""
 	// another configuration
