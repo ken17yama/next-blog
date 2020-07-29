bc6676413c5f3d37d630b6394a2e1d15a27d0bf6
ken17yama <ken17yama@users.noreply.github.com>
2020-07-29 17:00:15 +0000

    deploy: c8862aff2d26dae0c407ab7af7bb87df9e1b3178

--- _next/static/07IC2qwvda9V5UEfeSI6F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002F8ad097ed83495cbce3e589d4f575607319b8d4ab.2ee7f1b53b8bfd1265e5.js","static\u002Fcss\u002Fb97b8ca76f0fcb7c9bc9.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
