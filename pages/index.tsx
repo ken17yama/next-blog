8849808425123836f359ae0230f10514bf0d0a33
ken17yama <ken17yama@gmail.com>
2020-07-29 21:45:10 +0900

    GitHub Actionsへのデプロイの設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import Date from '../components/date'
-import * as utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 import { getSortedPostsData } from '../lib/posts'
 import { GetStaticProps } from 'next'
