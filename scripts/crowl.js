f83fe19ba96868ab4a8643423d0112f7f21c16df
ken17yama <ken17yama@gmail.com>
2020-08-02 01:54:00 +0900

    sitemap生成

--- scripts/crowl.js
+++ scripts/crowl.js
@@ -2,7 +2,7 @@ const SitemapGenerator = require('sitemap-generator');
 
 // create generator
 const generator = SitemapGenerator('https://ken17yama.github.io/next-blog/', {
-	filepath: './out/sitemap.xml',
+	filepath: '${GITHUB_WORKSPACE}/out/sitemap.xml',
 	stripQuerystring: false
 });
 
