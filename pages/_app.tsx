822dc94d473ebea50b7afee07fadbbae913e2fbd
ken17yama <ken17yama@gmail.com>
2020-08-01 23:01:07 +0900

    GAタグの設置

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
-import '../styles/global.scss'
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+
+import * as gtag from '../lib/gtag'
+import '../styles/global.scss'
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
