d0581b21aeb9d7dc5635c7e3c414a089a44c2e30
ken17yama <ken17yama@gmail.com>
2020-08-01 22:34:51 +0900

    サイトマップ生成

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+const EXTERNAL_DATA_URL = 'https://ken17yama.github.io/next-blog/posts';
 
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
